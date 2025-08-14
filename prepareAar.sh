df2d47ef78a40239016b42b1d77c08294910b938
craterone <craterone11@gmail.com>
2025-08-14 10:54:09 +0800

    complete prepareAar.sh

--- prepareAar.sh
+++ prepareAar.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 set -e
+
+VERSION=100
+
+RELEASE_VERSION=${VERSION}
+
+AAR_URL=https://github.com/0-u-0/dugon-webrtc-android/releases/download/${VERSION}/libwebrtc.aar
+
+echo VERSION=${VERSION}
+echo RELEASE_VERSION=${RELEASE_VERSION}
+echo AAR_URL=${AAR_URL}
+
+BUILD_DIR=build
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+curl -L -O ${AAR_URL}
+
+mvn install:install-file \
+    -Dfile=libwebrtc.aar \
+    -Dpackaging=aar \
+    -Dversion=${RELEASE_VERSION} \
+    -DgroupId=com.github.0-u-0 \
+    -DartifactId=dugon-webrtc-android
